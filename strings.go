93014c93a31eb64cc59df004aa4e5c1f1fa1bbea
alinosi <146401086+alinosi@users.noreply.github.com>
2025-06-13 09:17:17 +0700

    refactor: add comments to string manipulation examples for clarity

--- strings.go
+++ strings.go
@@ -6,10 +6,10 @@ import (
 )
 
 func main() {
-	fmt.Println(strings.Contains("Eko Kurniawan", "Eko"))
-	fmt.Println(strings.Split("Eko Kurniawan", " "))
-	fmt.Println(strings.ToLower("Eko Kurniawan"))
-	fmt.Println(strings.ToUpper("Eko Kurniawan"))
-	fmt.Println(strings.Trim("      Eko Kurniawan      ", " "))
-	fmt.Println(strings.ReplaceAll("Eko Kurniawan Eko Khannedy", "Eko", "Budi"))
+	fmt.Println(strings.Contains("Eko Kurniawan", "Eko")) // apakah string mengandung kata "Eko"
+	fmt.Println(strings.Split("Eko Kurniawan", " ")) // memecah setiap kata yang dipisahkan space menjadi slice
+	fmt.Println(strings.ToLower("Eko Kurniawan")) // konversi ke huruf kecil
+	fmt.Println(strings.ToUpper("Eko Kurniawan")) // konversi ke huruf besar
+	fmt.Println(strings.Trim("      Eko Kurniawan      ", " ")) // menghapus karakter pilihan di awal dan akhir kata
+	fmt.Println(strings.ReplaceAll("Eko Kurniawan Eko Khannedy", "Eko", "Budi")) // mengganti setiap kata Eko menjadi Budi
 }
