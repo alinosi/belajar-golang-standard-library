2543d268079621d3fe529304448fa76d386f6083
alinosi <mthoriqulfadli@gmail.com>
2025-06-19 09:18:08 +0700

    Add User struct and UserSlice implementation for sorting by age

---
+++ sort.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+type User struct {
+	Name string
+	Age  int
+}
+
+type UserSlice []User 
+
+func (s UserSlice) Len() int {
+	return len(s)
+}
+
+func (s UserSlice) Less(i, j int) bool {
+	return s[i].Age < s[j].Age
+}
+
+func (s UserSlice) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+func main() {
+	users := []User{
+		{"Noby", 30},
+		{"anton", 35},
+		{"alinosi", 25},
+		{"gunawan", 20},
+	}
+
+	// UserSlice dapat mengimplementasikan sort
+	
+	sort.Sort(UserSlice(users)) // konversi tipe data users ke UserSlice
+
+	fmt.Println(users)
+}
+
+/*
+ syarat untuk dapat menggunakan method dari package class 
+ kita harus mengikuti kontrak dari interface sort yaitu memiliki
+ 3 method : Len, Less, Swap
+*/
