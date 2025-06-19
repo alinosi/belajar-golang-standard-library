467e37322cc04f55071a831904b66f8ea632bef5
alinosi <mthoriqulfadli@gmail.com>
2025-06-19 22:03:32 +0700

    Add initial implementation of flag and os packages for command-line argument handling

---
+++ os.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	args := os.Args
+	for _, arg := range args {
+		fmt.Println(arg)
+	}
+
+	hostname, err := os.Hostname()
+	if err == nil {
+		fmt.Println(hostname)
+	} else {
+		fmt.Println("Error", err.Error())
+	}
+}
+
+// package os digunakan untuk operasi
+// yang melibatkan lingkunan sistem operasi
