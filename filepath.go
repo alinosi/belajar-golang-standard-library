83746c4fe608ed5c658eb92db7fde3a01a26fd3e
alinosi <mthoriqulfadli@gmail.com>
2025-06-24 20:07:08 +0700

    Add comments to clarify filepath.IsAbs and filepath.IsLocal usage

--- filepath.go
+++ filepath.go
@@ -9,7 +9,7 @@ func main() {
 	fmt.Println(filepath.Dir("hello/world.go"))
 	fmt.Println(filepath.Base("hello/world.go"))
 	fmt.Println(filepath.Ext("hello/world.go"))
-	fmt.Println(filepath.IsAbs("hello/world.go"))
-	fmt.Println(filepath.IsLocal("hello/world.go"))
+	fmt.Println(filepath.IsAbs("hello/world.go"))   // is absolute, Ex: C:\Users\User> (start from the top directory)
+	fmt.Println(filepath.IsLocal("hello/world.go")) // is relative
 	fmt.Println(filepath.Join("hello", "world", "main.go"))
 }
